bdbd8498a09b186c6a9948a1ca347e8ad292134d
Lakindu <86775461+LakinduVirajith@users.noreply.github.com>
2024-09-08 02:57:19 +0530

    language issues

--- lib/services/visual_service.dart
+++ lib/services/visual_service.dart
@@ -33,7 +33,7 @@ class VisualService {
         return VerbalQuestion.fromJson(data);
       } else {
         _commonService.handleHttpResponse(response, context, null,
-            {404: AppLocalizations.of(context)!.verbalServiceNoQuestionError});
+            {404: AppLocalizations.of(context)!.commonMessageNoQuestionError});
         return null;
       }
     } on http.ClientException {
